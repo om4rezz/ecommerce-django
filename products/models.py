3ed6086c1866e5d6b165a92fed2158491836f6ba
Omar Ezz Ed-Dien <om4rezz@MacBook-Pro.ninja.box>
2018-02-13 12:20:54 +0200

    create ModelManagers, ModelQuerySet and adjust them for Product model

--- products/models.py
+++ products/models.py
@@ -1,15 +1,32 @@
+from django.core.urlresolvers import reverse
 from django.db import models
 
 # Create your models here.
 
+class ProductQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def all(self, *args, **kwargs):
+        return self.get_queryset().active()
+
+
 class Product(models.Model):
-	title = models.CharField(max_length=120)
-	description = models.TextField(blank=True, null=True)
-	price = models.DecimalField(decimal_places=2, max_digits=20)
-	active = models.BooleanField(default=True)
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+    active = models.BooleanField(default=True)
 
+    objects = ProductManager()
 
+    def get_absolute_url(self):
+        # return "products/%s" % (self.pk)
+        return reverse("product_detail", kwargs={'pk': self.pk})
 
+    def __unicode__(self):
+        return self.title
 
-	def __unicode__(self):
-		return self.title
--- products/views.py
+++ products/views.py
@@ -1,10 +1,21 @@
+from django.http import Http404
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from django.shortcuts import render, get_object_or_404
-
+from django.utils import timezone
 # Create your views here.
 
 from .models import Product
 
+class ProductListView(ListView):
+    model = Product
+    queryset = Product.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        context['now'] = timezone.now()
+        print context
+        return context
 
 class ProductDetailView(DetailView):
     model = Product
