da9ef7cfafc4735e83c87f114aaca210b4c859e1
Omar Ezz Ed-Dien <om4rezz@MacBook-Pro.ninja.box>
2018-02-18 16:49:29 +0200

    search query using Q lookup

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import Http404
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
@@ -7,6 +8,7 @@ from django.utils import timezone
 
 from .models import Product
 
+
 class ProductListView(ListView):
     model = Product
     queryset = Product.objects.all()
@@ -14,12 +16,31 @@ class ProductListView(ListView):
     def get_context_data(self, *args, **kwargs):
         context = super(ProductListView, self).get_context_data(*args, **kwargs)
         context['now'] = timezone.now()
+        context['query'] = self.request.GET.get('q')
         print context
         return context
 
+    def get_queryset(self, *args, **kwargs):
+        qs = super(ProductListView, self).get_queryset(*args, **kwargs)
+        query = self.request.GET.get('q')
+        if query:
+            qs = self.model.objects.filter(
+                Q(title__icontains=query) |
+                Q(description__icontains=query)
+            )
+            try:
+                qs2 = self.model.objects.filter(
+                    Q(price=query)
+                )
+                qs = (qs | qs2).distinct()
+            except:
+                pass
+        return qs
+
+
 class ProductDetailView(DetailView):
     model = Product
-	# template_name = 'product.html'
+# template_name = 'product.html'
 
 
 def product_detail_view_func(request, id):
